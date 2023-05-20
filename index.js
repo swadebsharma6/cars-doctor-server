9a97cf016bfbf0f5ba98b99aee718525a2b0d30f
ph-swadeb-07 <swadebsharma2022@gmail.com>
2023-05-20 20:48:50 +0600

    added booking router api

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ async function run() {
 
         const options = {
           // Include only the `title` and `imdb` fields in the returned document
-          projection: { title: 1, price: 1, service_id: 1 },
+          projection: { title: 1, price: 1, service_id: 1, img:1,},
         };
 
         const result = await serviceCollection.findOne(query, options);
@@ -55,6 +55,20 @@ async function run() {
 
 
     // booking
+
+    app.get('/bookings', async(req, res) =>{
+      console.log(req.query.email);
+
+      let query = {};
+      if(req.query?.email){
+        query ={email: req.query.email}
+      }
+
+       const result = await bookingCollection.find(query).toArray();
+       res.send(result)
+    })
+
+
     app.post('/bookings', async(req, res) =>{
        const booking = req.body;
        console.log(booking);
